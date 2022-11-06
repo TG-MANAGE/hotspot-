739b86a28cde9c28de0ff62521c88e519a8318d1
AYUSH_ASH <107471238+shraajestayu@users.noreply.github.com>
2022-11-06 16:14:01 +0530

    Updated Active Voice By Ayush

--- YukkiMusic/plugins/sudo/ash.py
+++ YukkiMusic/plugins/sudo/ash.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from YukkiMusic import app
-from YukkiMusic.utils.database.memorydatabase import (get_active_chats, get_active_video_chats)
+from YukkiMusic.utils.database.memorydatabase import (active, activevideo)
 from YukkiMusic.misc import SUDOERS
 from YukkiMusic.utils.cmdforac import avoice
 #Imported Modules
@@ -16,8 +16,6 @@ from YukkiMusic.utils.cmdforac import avoice
 
 @app.on_message(avoice(["/ac"]) & SUDOERS)
 async def start(client: Client, message: Message):
-    ac_audio = get_active_chats
-    ac_video = get_active_video_chats
-    aud = str(len(ac_audio))
-    vid = str(len(ac_video))
+    ac_audio = active
+    ac_video = activevideo
     await message.reply_text(f"𝗕𝗼𝘁 𝗔𝗰𝘁𝗶𝘃𝗲 𝗖𝗵𝗮𝘁𝘀 𝗜𝗻𝗳𝗼 • 📟\n•━━━━━━━━━━━━━━━━━━•\n🎙•Aᴜᴅɪᴏ  » {ac_audio} Gʀᴏᴜᴘs\n•───────•\n🖥• Vɪᴅᴇᴏ » {ac_video} Gʀᴏᴜᴘs\n•──────•", quote=True)
