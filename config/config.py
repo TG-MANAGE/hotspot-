e8ad9a9eb6b851a572ef400da0259381d72457d2
AYUSH_ASH <107471238+shraajestayu@users.noreply.github.com>
2022-11-06 16:30:18 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -60,7 +60,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/life_codes")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/life_codes")
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
-AUTO_LEAVING_ASSISTANT = str("True")
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
 
 # Time after which you're assistant account will leave chats automatically.
 AUTO_LEAVE_ASSISTANT_TIME = int(getenv("AUTO_LEAVE_ASSISTANT_TIME"))  # Remember to give value in Seconds
