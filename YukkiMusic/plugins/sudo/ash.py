8326711cb50361324e75a5ed6e9ba711deeeb716
AYUSH_ASH <107471238+shraajestayu@users.noreply.github.com>
2022-11-06 16:06:02 +0530

    Update ash.py

--- YukkiMusic/plugins/sudo/ash.py
+++ YukkiMusic/plugins/sudo/ash.py
@@ -16,8 +16,8 @@ from YukkiMusic.utils.cmdforac import avoice
 
 @app.on_message(avoice(["/ac"]) & SUDOERS)
 async def start(client: Client, message: Message):
-    ac_audio = await get_active_chats
-    ac_video = await get_active_video_chats
+    ac_audio = get_active_chats
+    ac_video = get_active_video_chats
     aud = str(len(ac_audio))
     vid = str(len(ac_video))
     await message.reply_text(f"𝗕𝗼𝘁 𝗔𝗰𝘁𝗶𝘃𝗲 𝗖𝗵𝗮𝘁𝘀 𝗜𝗻𝗳𝗼 • 📟\n•━━━━━━━━━━━━━━━━━━•\n🎙•Aᴜᴅɪᴏ  » {aud} Gʀᴏᴜᴘs\n•───────•\n🖥• Vɪᴅᴇᴏ » {vid} Gʀᴏᴜᴘs\n•──────•", quote=True)
