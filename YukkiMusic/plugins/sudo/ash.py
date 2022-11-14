dd3e5a681ae06cc0b4b8ff2c06e7956429084154
AYUSH_ASH <107471238+shraajestayu@users.noreply.github.com>
2022-11-14 10:56:03 +0530

    Update ash.py

--- YukkiMusic/plugins/sudo/ash.py
+++ YukkiMusic/plugins/sudo/ash.py
@@ -1,6 +1,13 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from YukkiMusic import app
+import asyncio
+import math
+import os
+import shutil
+import socket
+
+
 from YukkiMusic.utils.database.memorydatabase import (active, activevideo)
 from YukkiMusic.misc import SUDOERS
 from YukkiMusic.utils.cmdforac import avoice
@@ -19,3 +26,21 @@ async def start(client: Client, message: Message):
     ac_audio = str(len(active))
     ac_video = str(len(activevideo))
     await message.reply_text(f"𝗕𝗼𝘁 𝗔𝗰𝘁𝗶𝘃𝗲 𝗖𝗵𝗮𝘁𝘀 𝗜𝗻𝗳𝗼 • 📟\n•━━━━━━━━━━━━━━━━━━•\n🎙•Aᴜᴅɪᴏ  » {ac_audio} Gʀᴏᴜᴘs\n•───────•\n🖥• Vɪᴅᴇᴏ » {ac_video} Gʀᴏᴜᴘs\n•──────•", quote=True)
+
+
+#--------------------------Clean_Commands------------------------#
+
+@app.on_message(avoice(["/fc"]) & SUDOERS)
+async def cleaning(client: Client, message: Message):
+    A = "downloads"
+    B = "raw_files"
+    C = "cache"
+    try:
+        shutil.rmtree(A)
+        shutil.rmtree(B)
+        shutil.rmtree(C)
+    except:
+        await message.reply_text(f"Failed To Delete Temp !!\nPlease Clean From VPS", quote=True)
+    await message.reply_text(f"Successfully Deleted Below Folders:\n -Downloads\n -raw_files\n -Cache", quote=True)
+
+
