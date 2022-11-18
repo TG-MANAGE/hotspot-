aa556ecb3b5d03e30334bdbef92c3b9c7cbbcbba
AYUSH_ASH <107471238+shraajestayu@users.noreply.github.com>
2022-11-18 12:23:07 +0530

    Update globalban.py

--- YukkiMusic/plugins/sudo/globalban.py
+++ YukkiMusic/plugins/sudo/globalban.py
@@ -25,7 +25,7 @@ UNGBAN_COMMAND = get_command("UNGBAN_COMMAND")
 GBANNED_COMMAND = get_command("GBANNED_COMMAND")
 
 
-@app.on_message(filters.command(GBAN_COMMAND) & filters.user(OWNER_ID))
+@app.on_message(filters.command(GBAN_COMMAND) & SUDOERS)
 @language
 async def gbanuser(client, message: Message, _):
     if not message.reply_to_message:
