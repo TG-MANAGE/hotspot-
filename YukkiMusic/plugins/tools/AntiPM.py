b484f982e3862f89e6236819d46a03568447041d
AYUSH_ASH <107471238+shraajestayu@users.noreply.github.com>
2022-12-21 15:13:38 +0530

    Update AntiPM.py

--- YukkiMusic/plugins/tools/AntiPM.py
+++ YukkiMusic/plugins/tools/AntiPM.py
@@ -8,3 +8,4 @@ ubapp = userbot.one
 @ubapp.on_message(filters.text & filters.private)
 async def echo(client, message):
 	await ubapp.send_message(LOG_GROUP_ID, "Message sent with!!")
+#Just For Testing
