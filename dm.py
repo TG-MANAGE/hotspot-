cf441b1f06a6d48c6758090874f560ac2b1c6c0d
AYUSH_ASH <107471238+shraajestayu@users.noreply.github.com>
2022-12-23 17:02:23 +0530

    Add files via upload

---
+++ dm.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+
+
+sepratelogin = Client("dmdemo",
+	api_id=int(API_ID),
+	api_hash=str(API_HASH),
+	session_string=str(STRING1),
+	no_updates=False)
+
+
+
+
+
+@sepratelogin.on_message(filters.text & filters.private)
+async def demo(client, message):
+	await message.reply("Https://t.me/life_codes")
+
+sepratelogin.run()
